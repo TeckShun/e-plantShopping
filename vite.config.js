8979ecfb6244b4018b5f38b987e6d2b38ef09398
Ang Teck Shun <angteckshun@gmail.com>
2024-07-13 18:08:23 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
+  base: "/https://github.com/TeckShun/e-plantShopping",
   plugins: [react()],
-
-  // build: {
-  //   rollupOptions: {
-  //       external: ['react', 'react-dom', 'react-router-dom'], // Add react-router-dom to external
-  //   },
-  // },
 })
